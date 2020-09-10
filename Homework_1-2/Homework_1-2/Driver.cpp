64e9966505ef3b0b91a8718ccc7d2f577080153a
John Deuschle <jonukaideuschle@gmail.com>
2020-09-10 18:51:08 -0500

    Created a driver

--- Homework_1-2/Homework_1-2/Driver.cpp
+++ Homework_1-2/Homework_1-2/Driver.cpp
@@ -1,4 +1,24 @@
 
+#include <string>
+#include <iostream>
+#include <iomanip>
 
-//Homework 1
 
+/*
+Authors: Paris Geist, Cole , Caleb Fengoli, John Deuschle (PLEASE CHECK SPELLING)
+CS 210-01
+Homework 1 Programming Assignment 1
+This program creates an unordered self-sorting linked list, using searches.
+*/
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+
+
+
+} //end main
